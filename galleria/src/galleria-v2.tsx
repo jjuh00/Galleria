30638787b7eb40f70f631c5db1ea83ecb8785194
jjuh00 <jamsen.juho@gmail.com>
2025-01-25 17:16:29 +0200

    Add galleria v2

---
+++ galleria/src/galleria-v2.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css"; 
+
+const kuvat: {src: string, alt: string}[] = [
+    { src: "./kuva_1.jpg", alt: "Kuva täysikuusta" },
+    { src: "./kuva_2.jpg", alt: "Kuva auringonlaskusta" },
+    { src: "./kuva_3.jpg", alt: "Kuva Tsuchinshan-ATLAS komeetasta" },
+    { src: "./kuva_4.jpg", alt: "ESP / Gran Canaria. Kuva auringonlaskusta hiekkarannalla" },
+    { src: "./kuva_5.jpg", alt: "GR / Crete. Kuva merelle päin" },
+    { src: "./kuva_6.jpg", alt: "GR / Crete. Kuva majakasta" },
+    { src: "./kuva_7.jpg", alt: "GR / Crete. Kuva aallokosta ja majakasta" },
+    { src: "./kuva_8.jpg", alt: "GR / Crete. Kuva veneistä" },
+    { src: "./kuva_9.jpg", alt: "Kuva kasvista Kaisaniemen kasvitieteellisessä puutarhassa" },
+    { src: "./kuva_10.jpg", alt: "Kuva kasvaista Kaisaniemen kasvitieteellisessä puutarhassa" },
+    { src: "./kuva_11.jpg", alt: "ESP / Gran Canaria. Kuva pienlentokoneesta" },
+    { src: "./kuva_12.jpg", alt: "Kaisaniemen kasvitieteellinen puutarha" },
+    { src: "./kuva_13.jpg", alt: "Valkeisenlampi" },
+    { src: "./kuva_14.jpg", alt: "Näkymä Neulamäen näköalatornista" },
+    { src: "./kuva_15.jpg", alt: "Valkeisenlampi" },
+    { src: "./kuva_16.jpg", alt: "GR / Crete. Kuva ulkokissasta" },
+    { src: "./kuva_17.jpg", alt: "Kaisaniemen kasvitieteellinen puutarha" },
+    { src: "./kuva_18.jpg", alt: "FIN / Tampere, Vapriikki" },
+    { src: "./kuva_19.jpg", alt: "FIN / Tampere, Vapriikki" },
+    { src: "./kuva_20.jpg", alt: "FIN / Tampere, Vapriikki" },
+    { src: "./kuva_21.jpg", alt: "Kuva kebabannoksesta" },
+    { src: "./kuva_22.jpg", alt: "Kuba Friends & Brgrs -annoksesta" },
+    { src: "./kuva_23.jpg", alt: "Kuva ravintolan jälkiruoasta" }
+];
+
+const GalleriaV2 = () => {
+    return (<h1>Moi</h1>);
+}
+
+export default GalleriaV2;
--- galleria/src/index.js
+++ galleria/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Galleria from './galleria';
+//import Galleria from './galleria'; 
+import GalleriaV2 from './galleria-v2.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Galleria />
+    {/*<Galleria /> */}
+    <GalleriaV2 />
   </React.StrictMode>
 );
 

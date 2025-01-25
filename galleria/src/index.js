e513ac08f56d753e8a58cfb6a799de870da53590
jjuh00 <jamsen.juho@gmail.com>
2025-01-25 18:01:56 +0200

    Update galleria v2

--- galleria/src/index.js
+++ galleria/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 //import Galleria from './galleria'; 
 import GalleriaV2 from './galleria-v2.tsx';
 
+/*
+  Jos haluat Gallerian (JavaScript version) näkyviin, ota import-lause Gallerialle pois kommenteista,
+  ja vaihda <GalleriaV2>-komponentin tilalle <Galleria>-komponentti
+*/
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<Galleria /> */}
     <GalleriaV2 />
   </React.StrictMode>
 );
